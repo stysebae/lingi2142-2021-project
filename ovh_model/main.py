0effc155b329576940b2cc1561f313da8edb1b61
stysebae <sophie.tysebaert@ulb.ac.be>
2020-10-15 20:13:06 +0200

    [MOD] Change structure repository

--- ovh_model/main.py
+++ ovh_model/main.py
@@ -53,7 +53,7 @@ class OVHTopology(IPTopo):
         self.addLink(ovh_r12, cogent_r1)
         self.addLink(ovh_r12, level3_r1)
 
-        # Setup the AS's
+        # Setup the ASes
         self.addAS(1, (ovh_r1, ovh_r2, ovh_r3, ovh_r4, ovh_r5, ovh_r6, ovh_r7, ovh_r8, ovh_r9, ovh_r10, ovh_r11, ovh_r12))
         self.addAS(2, (telia_r1))
         self.addAS(3, (cogent_r1))
